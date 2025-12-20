94e0787066afbbb9bece39f43691b9ba76ebb856
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-12-20 08:57:35 -0300

    feat: script on setup.sh to apply metrics server

--- infra/scripts/setup.sh
+++ infra/scripts/setup.sh
@@ -5,6 +5,10 @@ set -e
 
 CLUSTER_NAME="${CLUSTER_NAME:-url-shortener}"
 CONFIG_FILE="$(dirname "$0")/../kind/config.yaml"
+METRICS_SERVER_FILE="$(dirname "$0")/../kind/metrics-server.yaml"
+
+echo "Applying metrics server"
+kubectl apply -f "$METRICS_SERVER_FILE" 
 
 echo "Creating kind cluster: $CLUSTER_NAME"
 kind create cluster --name "$CLUSTER_NAME" --config "$CONFIG_FILE"
